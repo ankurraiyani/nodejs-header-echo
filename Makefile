b89a4dc398bfb7529676c5cdc6c66c9513f54c84
ramr <smitram@gmail.com>
2016-04-07 02:20:15 -0700

    Add scripts, cleanup makefile + expired certificates and update keys and cacerts.

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 
 IMAGE := ramr/header-test
+VERSION := v1
 
 
 all:	build
 
 build:	tlsconfig
 	docker build -t $(IMAGE) ./
+	docker tag $(IMAGE) $(IMAGE):$(VERSION)
 
 run:
 	@echo "  - Starting docker container for image $(IMAGE) ... "
@@ -14,18 +16,28 @@ run:
 clean:
 	@echo "  - Removing generated config files ... "
 	@rm -f config/cert.pem config/key.pem config/req.csr
+	@rm -f config/CA/cacert.pem config/CA/cakey.pem
+	@rm -f config/CA/index.txt* config/CA/serial* config/CA/01.pem
 
-	#  Shows errors for any containers running using that image.
-	@echo "  - Removing docker image for $(IMAGE) ... "
+	#  Shows errors for any containers running using those images.
+	@echo "  - Removing docker images for $(IMAGE) ... "
 	docker rmi $(IMAGE) || :
+	docker rmi $(IMAGE):$(VERSION) || :
 
 
 #  Internal targets.
-tlsconfig:	config/cert.pem config/key.pem
+tlsconfig:	cacertconfig certconfig
+
+cacertconfig:	config/cacert.pem config/cakey.pem
+certconfig:	config/cert.pem config/key.pem
 
 config/cert.pem:	config/key.pem
+config/cacert.pem:	config/cakey.pem
 
 config/key.pem:
+	./config/generate-tls-config.sh
+
+config/cakey.pem:
 	./config/generate-tls-config.sh
 
 .PHONY: 	clean
--- config/generate-tls-config.sh
+++ config/generate-tls-config.sh
@@ -2,6 +2,27 @@
 
 CFGDIR=$(cd -P -- "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 SUBJECT="/C=US/ST=CA/O=Security/OU=OpenShift3/CN=header.test"
+CASUBJECT="/C=US/ST=CA/O=Security/OU=OpenShift3 test CA/CN=header.test CA"
+CADIR="${CFGDIR}/CA"
+VALIDITY=7305  #  7305 days is 20 years and if the started docker
+               #  container is around at that time ... kudos!! Its time for
+               # a new cert!!
+
+
+#
+#  main():
+#
+cd ${CFGDIR}
+
+mkdir -p ${CADIR}/crl
+echo "01" > ${CADIR}/serial
+touch ${CADIR}/index.txt
+
+echo "  - Generating CA private key and certificate ... "
+OPENSSL=${CADIR}/ca.conf  \
+  openssl req -x509 -nodes -days ${VALIDITY} -newkey rsa:1024  \
+              -out ${CADIR}/cacert.pem  -outform PEM -keyout ${CADIR}/cakey.pem  \
+	      -subj "${CASUBJECT}"
 
 echo "  - Generating 2048 bit key ... "
 openssl genrsa -out "${CFGDIR}/key.pem" 2048
@@ -10,9 +31,7 @@ echo "  - Generating csr ... "
 openssl req -new -nodes -sha256 -key "${CFGDIR}/key.pem"  \
             -out "${CFGDIR}/req.csr" -subj "${SUBJECT}"
 
-echo "  - Generating cert ... "
-#  7305 days is 20 years and if the started docker container is around at that
-#  time, then it needs a new cert!! 
-openssl x509 -req -in "${CFGDIR}/req.csr" -signkey "${CFGDIR}/key.pem"  \
-             -days 7305 -out "${CFGDIR}/cert.pem"
-
+echo "  - Signing generated cert with CA ... "
+OPENSSL_CONF=${CADIR}/ca.conf  \
+  openssl ca -batch -notext -in "${CFGDIR}/req.csr"   \
+             -days ${VALIDITY}  -out "${CFGDIR}/cert.pem"
